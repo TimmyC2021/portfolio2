8af5c32912d0c5774d3e04c462de05253c3e59cc
TimmyC <77976627+TimmyC2021@users.noreply.github.com>
2021-04-06 22:21:18 +0100

    05 Started on the contact page

--- src/containers/About/About.jsx
+++ src/containers/About/About.jsx
@@ -7,14 +7,24 @@ const About = () => {
       <div className={styles.navbarSpacer}></div>
       <div className={`${styles.vertical_center} ${styles.homePage}`}>
         <h2 className={styles.aboutTitle}>About me:</h2>
-        <p>My name is Tim and I have lived and worked all my adult life here in Bristol</p>
-        <p>Most of this time I worked for an accountancy firm here in the area. 
+        <p>My name is Tim and I have lived and worked in Bristol since leaving university here.</p>
+        <p>Most of this time I worked for an accountancy firm here in the local area. 
           My job roles within that firm included:</p>
           <ul>
             <li>Accountant</li>
             <li>IT Manager</li>
             <li>Practice Manager</li>
           </ul>
+          <p>
+            The move to IT Manager was inevitable despite no formal training. I had a knack (persistance) 
+            to study things until I could make them work. This meant that for several years I had free reign to
+            explore and learn many aspects of technology.
+          </p>
+          <p>
+            Outside of work I have been known to do the odd bit of running. A love/hate relationship! I like the 
+            social side of it plus it justifies being able to eat and drink more. I don't enjoy the actual 
+            running part so much, especially if it involves getting my feet wet.
+          </p>
       </div>
     </div>
   )
--- src/containers/Contact/Contact.jsx
+++ src/containers/Contact/Contact.jsx
@@ -1,11 +1,15 @@
 import React from 'react';
 import styles from './Contact.module.scss';
+<script src="https://kit.fontawesome.com/a3c09e3783.js" crossorigin="anonymous"></script>
+
 
 const contact = () => {
   return (
-    <div className={styles.heroImage}>
-      <div className={styles.navbarSpacer}></div>
-      <h2>Contact me</h2>
+    <div>
+      <div className={styles.heroImage}>
+        <div className={styles.navbarSpacer}></div>
+        <h2>Contact me</h2>
+      </div>
     </div>
   )
 }
